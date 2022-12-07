258ea0a7dbb79c9925cf9fb48da5e5df12fcfbc3
Robin de Rooij <rderooij685@gmail.com>
2022-12-07 21:53:59 +0100

    increase version

--- pack.pl
+++ pack.pl
@@ -1,6 +1,6 @@
 name(jwt_io).
 title('JWT implementation').
-version('0.0.3').
+version('1.0.3').
 author('Can Bican','can@bican.net').
 author('Robin De Rooij','robin@terminusdb.com').
 maintainer('Can Bican','can@bican.net').
